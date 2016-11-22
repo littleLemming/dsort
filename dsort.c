b615b956f7ee9d3576808fc2e1ac3c65e9a95066
littleLemming <lemming0061@gmail.com>
2016-11-22 21:51:21 +0100

    essential methods for dsort

--- dsort.c
+++ dsort.c
@@ -13,12 +13,20 @@
  * 
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include<stdarg.h>
+
+
 /* === Constants === */
 
 /**
  * @brief max length for the strings in string_list - 1023 real characters and a \0 
  */
 #define LINE_SIZE (1024)
+
 
 /* === Macros === */
 
@@ -31,17 +39,64 @@
 #define DEBUG(...)
 #endif
 
+
 /* === Global Variables === */
 
-/** Name of the program */
+/* Name of the program */
 static const char *progname = "dsort"; /* default name */
+
 
 /* === Type Definitions === */
 
+
 /* === Prototypes === */
+
+/**
+ * free_resources
+ * @brief free allocated resources
+ */
+static void free_resources(void);
+
+/**
+ * bail_out
+ * @brief terminate program on program error
+ * @param exitcode exit code
+ * @param fmt format string
+ */
+static void bail_out(int exitcode, const char *fmt, ...);
+
 
 /* === Implementations === */
 
-int main(int argc, char **argv) {
+static void free_resources(void)
+{
+    /* clean up resources */
+}
 
+static void bail_out(int exitcode, const char *fmt, ...)
+{
+    va_list ap;
+
+    (void) fprintf(stderr, "%s: ", progname);
+    if (fmt != NULL) {
+        va_start(ap, fmt);
+        (void) vfprintf(stderr, fmt, ap);
+        va_end(ap);
+    }
+    if (errno != 0) {
+        (void) fprintf(stderr, ": %s", strerror(errno));
+    }
+    (void) fprintf(stderr, "\n");
+
+    free_resources();
+    exit(exitcode);
+}
+
+int main(int argc, char **argv) {
+    if(argc > 0) {
+		progname = argv[0];
+	}
+    if (argc != 3) {
+        bail_out(EXIT_FAILURE, "Usage: %s <command1> <command2>", progname);
+    }
 }
