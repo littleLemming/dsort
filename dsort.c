327a238effc1b573d418730a738062a67c0025ab
littleLemming <lemming0061@gmail.com>
2016-11-22 21:04:08 +0100

    structure of c-program

--- dsort.c
+++ dsort.c
@@ -13,6 +13,34 @@
  * 
  */
 
+/* === Constants === */
+
+/**
+ * @brief max length for the strings in string_list - 1023 real characters and a \0 
+ */
+#define LINE_SIZE (1024)
+
+/* === Macros === */
+
+/**
+ * @brief print debug messages if the debug flag is set 
+ */
+#ifdef ENDEBUG
+#define DEBUG(...) do { fprintf(stderr, __VA_ARGS__); } while(0)
+#else
+#define DEBUG(...)
+#endif
+
+/* === Global Variables === */
+
+/** Name of the program */
+static const char *progname = "dsort"; /* default name */
+
+/* === Type Definitions === */
+
+/* === Prototypes === */
+
+/* === Implementations === */
 
 int main(int argc, char **argv) {
 
