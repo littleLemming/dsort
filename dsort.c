9f3c9c30639573bf1014f54fd40450ab629c2d8f
littleLemming <lemming0061@gmail.com>
2016-11-22 20:30:24 +0100

    dsort.c with basic information

---
+++ dsort.c
@@ -0,0 +1,14 @@
+/**
+ * @file dsort.c
+ *
+ * @author Ulrike Schaefer 1327450
+ *
+ * 
+ * @brief dsort executes command1 and command2 and prints the duplicated output of those commands in a sorted order
+ *
+ * @details dsort execute command1 and command2, save the ouput of th commands in an array, 
+			this array gets sorted and only duplicated lines will be printed. 
+ *
+ * @date 22.11.2016
+ * 
+ */
